a0bac50b4dd5dd6e114861837195f3c61097667a
the-bay-kay <katie.risc@gmail.com>
2023-03-01 15:12:08 -0800

    Added backend support!
    Tested post and get methods with insomnia, successfully
    connected to MongoDB!

---
+++ backend/routes/posts.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+let Post = require('../models/posts.model');
+
+// handles http get requests on the /posts/ url path
+// gets all posts within the database (not efficient for many posts? may rewrite)
+router.route('/').get((req, res) => {
+    Post.find()
+        .then(posts => res.json(posts))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const images = req.body.images;
+    const tags = req.body.tags;
+    const date = Date.parse(req.body.date);
+
+    const newPost = new Post({
+        title,
+        body,
+        images,
+        tags,
+        date
+    });
+
+    newPost.save()
+        .then(() => res.json('Post added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// May not need this :)
+router.route('/:id').get((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            post.title = req.body.title;
+            post.body = req.body.body;
+            post.images = req.body.images;
+            post.tags = req.body.tags;
+            post.date = Date.parse(req.body.date);
+
+            post.save()
+                .then(() => res.json('Post updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+router.route('/delete/:id').post((req, res) => {
+    Post.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Post deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true }); // may need useCreateIndex: true
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB");
+});
+
+const postsRouter = require('./routes/posts');
+app.use('/posts', postsRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});

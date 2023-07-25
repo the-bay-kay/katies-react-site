77ae8bbac4918ed30496c0eb8be3368ede085346
the-bay-kay <katie.risc@gmail.com>
2023-07-25 16:16:03 -0700

    Added "Hero" splashpage
    TODO:
    - Add inner shadow to center photo
    - Add rest of the HomePage! Text Blurbs next

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,29 @@
+import HomeCSS from './page_css/Home.module.css'
+import heroRectangle from '../images/CenterSplash/HeroRectangle.svg'
+import heroPhoto from '../images/CenterSplash/HeroPhoto.JPEG'
 
 export function Home() {
     document.title = "Home";
 
+
+    /* Webpage order:
+     * Hero / Header
+     * Splash / "About Me"
+     * Portfolio: Should be a separate component!
+     * MessageMe: Also, should be separate component
+     */
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+       <div className={HomeCSS.hero}>
+            <h1>Software Engineer, Linguist, Artist</h1>
+            <div className={HomeCSS.heroImageWrapper}>
+                <img className= {HomeCSS.splashImage} src={heroPhoto} alt='Main Image Box' />
+            </div>
+       </div>
     );
 }
+
+/*
+
+                <img className={HomeCSS.centerImage} src={heroPhoto} alt='Main Image Box' />
+                <img className={HomeCSS.centerImage} src={heroRectangle} alt='Main Image Box' />
+*/

4097ef6dd2623025ad0cc38151abf50b0dfe6673
the-bay-kay <katie.risc@gmail.com>
2023-08-08 16:42:36 -0700

    Added textboxes for About Me / Info burbs
    TODO:
    - Move lengthy content paragraphs into JSON objects, setup
        a route that fetches these from the server
    - Write the "Abouut Me" blurb
    - Finish the rest of the homepage:
    - Add onHover effects to CSS after finishing main page,
        focus on polish.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,56 @@
+import { Link } from 'react-router-dom';
+
+import { aboutMeBlurb } from './About';
+
 import HomeCSS from './page_css/Home.module.css'
-import heroRectangle from '../images/CenterSplash/HeroRectangle.svg'
+import WavesTop from '../images/wavesOpacity.svg'
 import heroPhoto from '../images/CenterSplash/HeroPhoto.JPEG'
 
 export function Home() {
     document.title = "Home";
 
+    let blurb = aboutMeBlurb.substring(0, 320);
+    let lastWhitespaceIndex = blurb.lastIndexOf(' ');
+    const aboutBlurb = blurb.substring(0, lastWhitespaceIndex) + '...';
+    
 
-    /* Webpage order:
-     * Hero / Header
-     * Splash / "About Me"
-     * Portfolio: Should be a separate component!
-     * MessageMe: Also, should be separate component
-     */
     return (
-       <div className={HomeCSS.hero}>
-            <h1>Software Engineer, Linguist, Artist</h1>
-            <div className={HomeCSS.heroImageWrapper}>
-                <img className= {HomeCSS.splashImage} src={heroPhoto} alt='Main Image Box' />
-            </div>
-       </div>
+        <div>
+            {/* Main Hero Splash*/}
+            <section className={HomeCSS.hero}>
+                <h1>Software Engineer, Linguist, Artist</h1>
+                <div className={HomeCSS.heroImageWrapper}>
+                    <img className= {HomeCSS.splashImage} src={heroPhoto} alt='Main Box' />
+                </div>
+            </section>
+            {/* Info Blurb */}
+            <div className={HomeCSS.sectionBuffer}/>
+            <section className={HomeCSS.blurb}>
+                <img className={HomeCSS.waveDivider} src={WavesTop} alt='Test'/>
+                <div className={HomeCSS.blurbContainer}>
+                    <div className={HomeCSS.leftBlurbWrapper}>
+                        <div className={HomeCSS.blurbBox}>
+                            <h2>About Me</h2>
+                            <p>{aboutBlurb}</p>
+                            <div class={HomeCSS.buttonContainer}> {/* Extra Div prevents whole grid cell from being a link */}
+                                <Link to="/about">
+                                    <button>More</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={HomeCSS.rightBlurbWrapper}>
+                        <div className={HomeCSS.blurbBox}>
+                            <h2> What the heck?</h2>
+                        <p>Here's some more filler text. Aren't I the best software engineer?</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* Portfolio */}
+            {/* Contact Me */}
+        </div>
+
     );
 }
 
-/*
-
-                <img className={HomeCSS.centerImage} src={heroPhoto} alt='Main Image Box' />
-                <img className={HomeCSS.centerImage} src={heroRectangle} alt='Main Image Box' />
-*/
